782007638b03ff39ce087cd0ddf75289e8623258
LibeerDev <libeergithub@gmail.com>
2023-11-09 19:20:37 +0100

    billing step boilerplate

---
+++ js/form.js
@@ -0,0 +1,45 @@
+let currentStep = 0;
+
+function updateButton(currentStep) {
+    const allStepButtons = document.querySelectorAll('.step-btn');
+
+    allStepButtons.forEach((button) => {
+        button.classList.remove('selected');
+    });
+
+    allStepButtons[currentStep].classList.toggle('selected');
+}
+
+function goToStep(goToStepIndex) {
+
+    document.getElementById(`step${currentStep}`).classList.add('hidden');
+
+    // increment and show the next step window
+    currentStep = goToStepIndex;
+
+
+    document.getElementById(`step${goToStepIndex}`).classList.remove('hidden');
+
+    updateButton(currentStep);
+
+}
+
+function nextStep() {
+    const allStepButtons = document.querySelectorAll('.step-btn');
+
+
+    // hide current step window
+    document.getElementById(`step${currentStep}`).classList.add('hidden');
+
+    // increment and show the next step window
+    if (document.getElementById(`step${currentStep + 1}`) !== null) {
+        currentStep++;
+
+
+        document.getElementById(`step${currentStep}`).classList.remove('hidden');
+
+
+        updateButton(currentStep);
+    }
+
+};
--- libeer_notes.md
+++ libeer_notes.md
@@ -1,3 +1,7 @@
 Time reading readme instructions on how to make the projects: 30 minutes
 Estimated time spent on project: 9 hours
-Real time spent: 3 hours
+Real time spent: 4 hours
+
+JavaScript: 1 hours;
+Step1: Personal info front-end: 3 hours
+Step 2: 
